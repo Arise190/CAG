c24bcac18da37bbd53a886d938031bdaee9fc9e4
Reaun <warz1569@gmail.com>
2026-07-01 00:39:24 +0700

    new3

--- fhnxx.py
+++ fhnxx.py
@@ -12,11 +12,26 @@ while True:
         grade = float(grade)
         credit = int(credit)
 
-        if not (0 <= grade <= 4):
+        if grade < 0 or grade > 4:
             print("เกรดไม่ถูกต้อง")
             continue
 
         subjects.append([subject, grade, credit])
 
-    except:
+    except ValueError:
         print("รูปแบบไม่ถูกต้อง")
+
+total_grade = 0
+total_credit = 0
+
+print("\nรายวิชา")
+
+for sub in subjects:
+    print(f"วิชา : {sub[0]} เกรด : {sub[1]} หน่วยกิต : {sub[2]}")
+    total_grade += sub[1] * sub[2]
+    total_credit += sub[2]
+
+if total_credit > 0:
+    print(f"\nGPA : {total_grade / total_credit:.2f}")
+else:
+    print("ไม่มีข้อมูล")
